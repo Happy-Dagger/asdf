3a69063daaab1afbab728816645b06b6ef743f92
Happy-Dagger <46727190+Happy-Dagger@users.noreply.github.com>
2021-07-08 15:36:33 -0500

    Update snap.js

--- snap.js
+++ snap.js
@@ -1,1 +1,3 @@
-setTimeout(function(){$('body').removeAttr('style').html('<img src="https://image.ibb.co/byWcEv/rtaImage.jpg">').css('text-align','center').css('margin','125px');},2500);
+function msg(){  
+ alert("Hello world!");  
+} 
